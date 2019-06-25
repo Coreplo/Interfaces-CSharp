3d0bce7182e5c69d0481cb77a2d91f224ea00481
Coreplo <coreplo@gmail.com>
2019-06-25 19:59:09 +0200

    testing how interfaces works and checking whats the difference between fields and properties

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,12 +10,48 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+           
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+           
         }
     }
+
+    // interface
+    // a completetely abstract class containing only abstract members
+
+    public interface  IShape
+    {
+        void Draw(); 
+    }
+    // once definded a class as an interface there is no need to put abstract in front of each method
+    // also all methods are public by default and cannot be changed
+    // also interfaces can have methods and properties but cannot contain fields (variables)
+    // thats because an interface is a definition and not an implementation
+    // I assume (but i might be wrong that it can have
+    // String name;
+    // but not 
+    // String Name = "Tony Strack";
+    // meh.. now me test it
+
+    public interface IMayHaveVariables
+    {
+        void Fart();
+        string name;
+    }
+
+    // nah.. does not work
+    // String Name is considered a field;
+    // now I guess properties are like:
+
+    public interface IMayHaveProperties
+    {
+        void Fart();
+        string name { get;set; }
+    }
+
+    //Yay! that Vorks!
+    //properties are those weird things with get and set that combine fields and methods
+
 }
